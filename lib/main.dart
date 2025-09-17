a46ff0a18b8b7c97b466180c7bb08df4e6dbba4d
sannn-s <gileejadahne@gmail.com>
2025-09-18 02:05:03 +0700

    add new images and modify text in KotakBiruJempol widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,48 +9,76 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Icon with Text',
+    return MaterialApp( 
+      title: 'Row and Column',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Icon + Text'),
+          title: Text('Row and Column'),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.favorite, color: Colors.red, size: 50),
-                  Text("Love"),
-                ],
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.star, color: Colors.amber, size: 50),
-                  Text("Star"),
-                ],
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.home, color: Colors.blue, size: 50),
-                  Text("Home"),
-                ],
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.person, color: Colors.green, size: 50),
-                  Text("Profile"),
-                ],
-              ),
-            ],
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                 KotakBiruJempol(icon: Icons.favorite, teks: "Love 1"),
+                  KotakBiruJempol(icon: Icons.favorite, teks: "Love 3"),
+              ],
+            ),
+            SizedBox(width: 10),
+            Column(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                KotakBiruJempol(icon: Icons.favorite, teks: "Like 2"),
+                KotakBiruJempol(icon: Icons.favorite, teks: "Love 4"),
+              ],
+            ),
+          ],
         ),
       ),
+      ),
     );
+  }
+}
+
+class KotakBiruJempol extends StatelessWidget {
+
+  final IconData icon;
+  final String teks;
+
+   KotakBiruJempol({required this.icon, required this.teks});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+     Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(color: Colors.black, width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ), 
+      child: Icon(
+            icon,
+            color: Colors.red,
+            size: 40,
+          ),
+     ),
+          SizedBox(height: 5),
+          Text(
+            teks,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+      ],
+    );  
   }
 }
